95c2f5ac076a91787be7d7fb040abb7bcd8b9131
yousef maraqa <86913045+MuhyeAlRaie@users.noreply.github.com>
2023-08-26 20:50:29 +0300

    Adding the rest of the code

---
+++ qprostask/src/qprostask/RandomNames.java
@@ -0,0 +1,9 @@
+package qprostask;
+
+import java.util.Random;
+
+public class RandomNames {
+	Random random = new Random();
+	String[] names = {"Ali@321", "KhalidDark", "JameelJink", "TahseenTEST"};
+	int randNames = random.nextInt(4); 
+}
